e02d5dac2646c63994bf312f36dc602c818cd627
madlabs-ally <madalitsosimbeye173@gmail.com>
2022-09-08 16:32:55 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,16 @@
 
 /**
  *
- *  main - prints the string 
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *  Description: using the main function
- *  This program prints "Programming is like building a multilingual puzzle"
- *  Return: 0
- */
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
 
 int main(void)
 
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 }
