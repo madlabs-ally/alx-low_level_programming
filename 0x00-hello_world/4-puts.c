8d3320b3ba32205724ba285703246f72b07ede87
madlabs-ally <madalitsosimbeye173@gmail.com>
2022-09-08 16:29:27 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  main - prints the string 
@@ -14,9 +12,6 @@
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }
