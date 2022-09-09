0751a401cd8494c693c9f9d01afb0399ca64b34d
madlabs-ally <madalitsosimbeye173@gmail.com>
2022-09-08 17:54:15 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,8 @@
  * this prints "Programming is like building a multiligual puzzle
  * Return: 0
  */
-int main(void) 
-{
+ int main(void) 
+ {
         printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+ }
