dbbc916a541e6719835beba998cab7001acf2d17
Madalitso Simbeye <madalitsosimbeye173@gmail.com>
2022-09-21 16:38:56 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,6 @@
 /**
  * _strcat - function that concatenates
  *          two strings.
- *
  * @dest: pointer to des input
  * @src: pointer to src input
  *
@@ -13,18 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int e, e2;
+	int e = 0, e2 = 0;
 
-	/*find the size of dest array*/
-	while (dest[e]){
-		e++;
-          }
-	/* iterate through each src array value without the null byte*/
-	for (e2 = 0; src[e2] ; e2++){
+	while (dest[e++])
+		e2++;
 
-		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
-		dest[e++] = src[e2];
-        }
+	for (e = 0; src[e] ; e++)
+		dest[e2++] = src[e];
        
 	return (dest);
 }
