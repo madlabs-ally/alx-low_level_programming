af5bc3f051ae9d7864cebb2f8c1889849d906e2b
Madalitso Simbeye <madalitsosimbeye173@gmail.com>
2022-09-21 17:42:43 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,23 +3,22 @@
 /**
  * leet - encodes a string into 1337
  * @s: string to encode
+ *
  * Return: address of s
  */
 
 char *leet(char *s)
-
 {
-	int k, l;
+	int i, j;
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
-	for (k = 0; *(s + k); k++)
+	for (i = 0; *(s + i); i++)
 	{
-		for (l = 0; l <= 9; l++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (a[l] == *(s + l))
-				*(s + k) = b[l];
-		}
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];		}
 	}
 	return (s);
 }
