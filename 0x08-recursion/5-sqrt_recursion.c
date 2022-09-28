b1da89f191a2688330a8e6de3ff473f73eec0db0
Madalitso Simbeye <madalitsosimbeye173@gmail.com>
2022-09-28 16:30:21 -0700

    sqrt rec

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
 		return (n);
-	return (_sqrt-rec(0, n));
+	return (_sqrt(0, n));
 }
 
 /**
@@ -22,11 +22,11 @@ int _sqrt_recursion(int n)
  * Return: the square root of n
  */
 
-int _sqrt-rec(int n, int x)
+int _sqrt(int n, int x)
 {
 	if (n > x / 2)
 		return (-1);
 	else if (n * n == x)
 		return (n);
-	return (_sqrt-rec(n + 1, x));
+	return (_sqrt(n + 1, x));
 }
