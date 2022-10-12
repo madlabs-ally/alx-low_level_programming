d4f013b341b62afbb1e16e94ad110b158dc6b19f
Madalitso Simbeye <madalitsosimbeye173@gmail.com>
2022-10-12 16:43:27 -0700

    int_intex

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include "function_pointers.h"
-
 /**
  * array_iterator - Executes a function as a parameter
  * on each element of an array
@@ -12,13 +9,30 @@
  * @action: Pointer to a function
  */
 
-void array_iterator(int *array, size_t size, void (*action)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
-	size_t m;
+    int i res;
 
-	if (!action || !array)
-		return;
-
-	for (m = 0; m < size; m++)
-		action(array[m]);
+    res = -1;
+    if(array && cmp)
+    {
+           if (size <= 0)
+           {
+                    return (res);
+           }
+           for (i = 0; i < size; i++)
+           {
+                  cmp(array[i]);
+                  if (cmp(array[i]) > 0)
+                  {
+                     res = i;
+                     break;
+                  }
+                  if ((cmp(array[i]) == (-1)))
+                  {
+                     return(res);
+                  }
+           }
+    }
+    return(res);
 }
