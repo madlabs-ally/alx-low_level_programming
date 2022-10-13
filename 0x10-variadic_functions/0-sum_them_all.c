408a379b4c765fcc00adbc7ec8aff9b2d1e82398
Madalitso Simbeye <madalitsosimbeye173@gmail.com>
2022-10-13 13:53:21 -0700

    Beauty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int m;
+	unsigned int i;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
 	va_start(valist, n);
-	for (m = 0; m < n; m++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
 	va_end(valist);
 
