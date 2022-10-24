b2965b49c8c86e7a7deae72ac44dfbeae7830030
madlabs-ally <madalitsosimbeye173@gmail.com>
2022-10-24 14:03:21 -0700

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * listint_len  -  Return The number of elements
+
+ * of a listint_t list.
+ * @h: head of the list
+ *
+ * Return: The number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t node = 0;
+
+	if (h == NULL)
+		return (node);
+
+	while (h != NULL)
+	{
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
